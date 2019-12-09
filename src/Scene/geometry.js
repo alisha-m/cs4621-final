3d23586da8f4d440388df27406433442c326c064
Jake Sanders <jas2223@cornell.org>
2019-12-09 01:49:43 -0500

    surface is a square

--- src/Scene/geometry.js
+++ src/Scene/geometry.js
@@ -13,3 +13,29 @@ class Face {
         this.indices = vec3.fromValues(a, b, c);
     }
 }
+
+/*
+class Geometry {
+    constructor() {
+        this.vertices = [];
+        this.uvs = [];
+        this.normals = [];
+        this.faces = [];
+    }
+}
+
+class Face {
+    // TODO: perhaps have more stuff here
+    constructor() {
+        this.vertices = [0, 0, 0];
+        this.uvs = [0, 0, 0];
+        this.normals = [0, 0, 0];
+    }
+
+    setVertex(vertexIndex, positionIndex, uvIndex, normalIndex) {
+		this.vertices[vertexIndex] = positionIndex;
+		this.uvs[vertexIndex] = uvIndex;
+		this.normals[vertexIndex] = normalIndex;
+	}
+}
+*/
