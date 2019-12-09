b54264e88b9afee641b8277583629d104ac912c4
Alisha Mithal <alisha.mithal@gmail.com>
2019-12-09 01:54:41 -0500

    okay sorry this should be good

--- src/Scene/camera.js
+++ src/Scene/camera.js
@@ -9,8 +9,10 @@ class Camera extends SceneObject {
         this.aspectRatio = aspectRatio;
         this.near = near;
         this.far = far;
+
         this.defaultCamDir = vec3.fromValues(1, 0, 0);
         this.camUp = vec3.fromValues(0, 0, 1);
+        this.currentHeading = 0.0;
     }
 
     getCamDir() {
@@ -19,5 +21,37 @@ class Camera extends SceneObject {
         vec3.rotateY(camDir, camDir, vec3.create(), this.transform.rotation[1]);
         vec3.rotateZ(camDir, camDir, vec3.create(), this.transform.rotation[2]);
         return camDir;
+    }
+
+    turnRight(speed) {
+      let moveAmount = vec3.create();
+      let dir = vec3.create();
+      vec3.copy(dir,scene.camera.defaultCamDir);
+      console.log(dir);
+      vec3.cross(dir, dir, scene.camera.camUp);
+      vec3.scale(moveAmount, dir, speed);
+      vec3.add(scene.camera.transform.position, scene.camera.transform.position, moveAmount);
+    }
+
+    turnLeft(speed) {
+      let moveAmount = vec3.create();
+      let dir = vec3.create();
+      vec3.copy(dir,scene.camera.defaultCamDir);
+      vec3.cross(dir, dir, scene.camera.camUp);
+      vec3.scale(moveAmount, dir, speed);
+      vec3.sub(scene.camera.transform.position, scene.camera.transform.position, moveAmount);
+
+    }
+
+    goForward(speed) {
+      let moveAmount = vec3.create();
+      vec3.scale(moveAmount, scene.camera.defaultCamDir, speed);
+      vec3.add(scene.camera.transform.position, scene.camera.transform.position, moveAmount);
+    }
+
+    goBackward(speed) {
+      let moveAmount = vec3.create();
+      vec3.scale(moveAmount, scene.camera.defaultCamDir, speed);
+      vec3.sub(scene.camera.transform.position, scene.camera.transform.position, moveAmount);
     }
 }
